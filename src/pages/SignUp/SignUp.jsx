d27bff6e363cd244d4697266348755659c9168ba
lokesh143b <138014740+lokesh143b@users.noreply.github.com>
2025-04-13 17:29:43 +0530

    Update SignUp.jsx

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./Signup.css"
 
 function Signup() {
   const [form, setForm] = useState({ email: '', password: '' });
